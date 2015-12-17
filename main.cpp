a56ff6fdd04936f8cc28babbd5192bcab4c68f6c
Muto <leandromuto@Leandros-MacBook-Pro.local>
2015-12-17 21:28:47 -0200

    fixed errors

--- main.cpp
+++ main.cpp
@@ -1,63 +1,4 @@
 
-//
-//folha *criaFolha(folha *ini, int num, folha *esq, folha *dir) { // esquerda e direita podem ser null
-//	folha *nova = (folha *) malloc(sizeof(folha));
-//	
-//	if(!nova) {
-//		printf("Falta de memória ao criar índice.\n");
-//		exit(0);
-//	}
-//	
-//	nova->num = num;
-//	nova->esq = esq;
-//	nova->dir = dir;
-//	nova->balanceamento = altura(esq) - altura(dir);
-//	
-//	nova->tupla = (tupla *) malloc(sizeof(cel));
-//	nova->tupla->prox = nullptr;
-//	
-//	printf("\n\n\nÍndice %d inserido com sucesso!\n", num);
-//	
-//	return nova;
-//} // OK
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
  *  Árvore AVL
  *
@@ -68,10 +9,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <cstdlib>
-#include <iostream>
-#include <unistd.h>
-#include <term.h>
 
 typedef struct cel {
 	char nome[100];
@@ -90,12 +27,12 @@ typedef struct no {
 
 folha *buscaFolha(folha *ini, int numFolha) {
 	folha *aux = ini;
-	
+
 	if(ini == nullptr) {
 		printf("\n\n\nRaiz não encontrada.\n\n\n");
 		return nullptr;
 	}
-	
+
 	while(aux != nullptr) {
 		if(numFolha == aux->num) {
 			return aux;
@@ -107,9 +44,9 @@ folha *buscaFolha(folha *ini, int numFolha) {
 			aux = aux->dir;
 		}
 	}
-	
+
 	printf("Índice %d não encontrada.\n", numFolha);
-	
+
 	return aux;
 } // OK
 
@@ -117,71 +54,71 @@ void imprimeNomes(folha *ini, int numFolha) {
 	folha *aux = ini;
 	folha *f = buscaFolha(aux, numFolha);
 	tupla *auxTu = f->tupla;
-	
+
 	if(auxTu->prox == nullptr) {
 		printf("Nenhum nome na lista da folha %d.\n", numFolha);
 		return;
 	}
-	
+
 	printf("\n\n|---Índice %d---|\n", numFolha);
-	
+
 	auxTu = auxTu->prox;
-	
+
 	while(auxTu != nullptr) {
 		printf("| Nome: %s\n", auxTu->nome);
 		auxTu = auxTu->prox;
 	}
-	
+
 	printf("|---Total---%d -|\n\n", f->countNomes);
 } // OK
 
 void ordenaNome(tupla *t) {
 	tupla *aux;
-	
+
 	if(t->prox == nullptr) {
 		printf("Lista vazia.");
 		return;
 	}
-	
+
 	aux = t->prox;
 	while(aux->prox != nullptr) {
 		int cmp = strcmp(aux->nome, aux->prox->nome);
-		
+
 		if(cmp > 0) {
 			char cp[100];
 			strcpy(cp, aux->nome);
 			strcpy(aux->nome, aux->prox->nome);
 			strcpy(aux->prox->nome, cp);
-			
+
 			aux = t->prox;
 		}
-		
+
 		aux = aux->prox;
 	}
 } // OK
 
 tupla *insereLista(folha *ini, int numFolha, char nome[]) { // insere nome na lista encadeada da folha que inserir como parametro
-	
+
 	folha *folhaExiste = buscaFolha(ini, numFolha);
-	
+
 	if(folhaExiste == nullptr) { // verificando se a folha existe, se não sai fora
 		return nullptr;
 	}
-	
+
 	tupla *t, *aux;
 	t = (tupla *) malloc(sizeof(tupla)); // criando nova tupla
 	strcpy(t->nome, nome); // copia nome do parâmetro pra tupla->nome
-	
+
 	aux = folhaExiste->tupla; // aux recebe cabeça das tuplas da struct folha
-	
+
 	t->prox = folhaExiste->tupla->prox; // aponta pra qual a cabeça tava apontando
 	aux->prox = t; // cabeça aponta pra nova
 	printf("Nome \"%s\" inserido no índice %d.\n\n", nome, numFolha);
-	
+
 	folhaExiste->countNomes++;
-	
+
 	ordenaNome(folhaExiste->tupla);
-	
+
 	return t;
 } // OK
 
@@ -193,7 +130,7 @@ folha *novoNo(int num) {
 	novo->altura = 1;
 	novo->tupla = (tupla *) malloc(sizeof(tupla));
 	novo->tupla->prox = nullptr;
-	
+
 	printf("Nó inserido com sucesso!\n\n\n");
 	return novo;
 } // OK
@@ -208,15 +145,15 @@ int altura(folha *n) {
 folha *rotacionarDireita(folha *y) {
 	folha *x = y->esq;
 	folha *T2 = x->dir;
- 
+
 	// faz rotação
 	x->dir = y;
 	y->esq = T2;
- 
+
 	// atualiza altura
 	y->altura = std::max(altura(y->esq), altura(y->dir))+1;
 	x->altura = std::max(altura(x->esq), altura(x->dir))+1;
- 
+
 	// retorna nova raiz
 	return x;
 } // OK
@@ -224,15 +161,15 @@ folha *rotacionarDireita(folha *y) {
 folha *rotacionarEsquerda(folha *x) {
 	folha *y = x->dir;
 	folha *T2 = y->esq;
- 
+
 	// faz rotação
 	y->esq = x;
 	x->dir = T2;
- 
+
 	// altualiza altura
 	x->altura = std::max(altura(x->esq), altura(x->dir))+1;
 	y->altura = std::max(altura(y->esq), altura(y->dir))+1;
- 
+
 	// retorna nova raiz
 	return y;
 } // OK
@@ -248,30 +185,30 @@ folha *insereNo(folha *fol, int num) {
 	if(fol == nullptr) {
 		return novoNo(num);
 	}
-	
+
 	if(num < fol->num) {
 		fol->esq = insereNo(fol->esq, num);
 	}
 	else {
 		fol->dir = insereNo(fol->dir, num);
 	}
-	
+
 	fol->altura = std::max(altura(fol->esq), altura(fol->dir)) + 1; // atualiza a altura do anterior
-	
+
 	int bal = balanceamento(fol); // pega o valor do balanceamento do anterior para ver para qual lado rotacionar
-	
+
 	// Rotação dupla a esquerda
 	if (bal > 1 && num < fol->esq->num) {
 		printf("Rotação dupla a esquerda.\n");
 		return rotacionarDireita(fol);
 	}
- 
+
 	// Rptação dupla a direita
 	if (bal < -1 && num > fol->dir->num) {
 		printf("Rotação dupla a direita.\n");
 		return rotacionarEsquerda(fol);
 	}
- 
+
 	// Rotação a esquerda e depois direita
 	if (bal > 1 && num > fol->esq->num)
 	{
@@ -279,7 +216,7 @@ folha *insereNo(folha *fol, int num) {
 		printf("Rotação a esquerda e depois a direita.\n");
 		return rotacionarDireita(fol);
 	}
- 
+
 	// Rotação a esquerda e depois a direita
 	if (bal < -1 && num < fol->dir->num)
 	{
@@ -287,7 +224,7 @@ folha *insereNo(folha *fol, int num) {
 		printf("Rotação a direita e depois a esquerda.\n");
 		return rotacionarEsquerda(fol);
 	}
-	
+
 	return fol;
 } // OK
 
@@ -302,10 +239,10 @@ void ordemArvore(folha *root) {
 }
 
 int main() {
-	
+
 	int opc = 9;
 	folha *root = nullptr;
-	
+
 	while(opc != 0) {
 		printf("\n----------- Menu -------------------\n");
 		printf("| 1 - Inserir nó ------------------|\n");
@@ -314,18 +251,18 @@ int main() {
 		printf("| 4 - Exibir árvore em ordem ------|\n");
 		printf("| 0 - Sair ------------------------|\n");
 		printf("------------------------------------\n");
-		
+
 		printf("Escolha uma opção: ");
 		scanf("%d", &opc);
-		
+
 		switch (opc) {
 			case 1: {
 				int ind;
 				printf("Índice do nó a ser inserido: ");
 				scanf("%d", &ind);
-				
+
 				root = insereNo(root, ind);
-				
+
 				break;
 			}
 			case 2: {
@@ -335,24 +272,24 @@ int main() {
 				printf("Em qual índice deseja colocar o nome?\n");
 				printf("Índice: ");
 				scanf("%d", &ind);
-				
+
 				folha *aux = buscaFolha(root, ind);
-				
+
 				while(aux == nullptr) {
 					printf("\n\n\nTente novamente.\n");
 					printf("Em qual índice deseja colocar o nome?\n");
 					printf("Índice: ");
 					scanf("%d", &ind);
-					
+
 					aux = buscaFolha(root, ind);
 				}
-				
+
 				printf("Nome que deseja colocar: ");
 				scanf("%s", &nome);
-				
+
 				insereLista(root, ind, nome);
 				imprimeNomes(root, ind);
-				
+
 				break;
 			}
 			case 3: {
@@ -361,9 +298,9 @@ int main() {
 				printf("De qual índice deseja imprimir?\n");
 				printf("Índice: ");
 				scanf("%d", &ind);
-				
+
 				folha *f = buscaFolha(root, ind);
-				
+
 				if(f == nullptr) {
 					//					printf("\n\n\nVocê escolheu imprimir os nomes de um índice.\n");
 					//					printf("De qual índice deseja imprimir?\n");
@@ -373,7 +310,7 @@ int main() {
 					//					f = buscaFolha(root, ind);
 					return 0;
 				}
-				
+
 				imprimeNomes(root, ind);
 				break;
 			}
@@ -393,19 +330,5 @@ int main() {
 			}
 		}
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
